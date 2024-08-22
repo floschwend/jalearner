3847c46fa901f3d008733314c84bf49c604ff458
flo <flo.schwend@gmail.com>
2024-08-22 16:01:55 +0200

    add sign requirement

--- config/index.tsx
+++ config/index.tsx
@@ -1,5 +1,7 @@
 import { defaultWagmiConfig } from '@web3modal/wagmi/react/config'
-
+import { getCsrfToken, signIn, signOut, getSession } from 'next-auth/react'
+import type { SIWEVerifyMessageArgs, SIWECreateMessageArgs, SIWESession } from '@web3modal/siwe'
+import { createSIWEConfig, formatMessage } from '@web3modal/siwe'
 import { cookieStorage, createStorage } from 'wagmi'
 import { base, baseSepolia } from 'wagmi/chains'
 
@@ -25,4 +27,57 @@ export const config = defaultWagmiConfig({
   storage: createStorage({
     storage: cookieStorage
   }),
-})
+});
+
+export const siweConfig = createSIWEConfig({
+    getMessageParams: async () => ({
+      domain: typeof window !== 'undefined' ? window.location.host : '',
+      uri: typeof window !== 'undefined' ? window.location.origin : '',
+      chains: [base.id, baseSepolia.id],
+      statement: 'Please sign with your account'
+    }),
+    createMessage: ({ address, ...args }: SIWECreateMessageArgs) => formatMessage(args, address),
+    getNonce: async () => {
+      const nonce = await getCsrfToken()
+      if (!nonce) {
+        throw new Error('Failed to get nonce!')
+      }
+  
+      return nonce
+    },
+    getSession: async () => {
+      const session = await getSession()
+      if (!session) {
+        throw new Error('Failed to get session!')
+      }
+  
+      const { address, chainId } = session as unknown as SIWESession
+  
+      return { address, chainId }
+    },
+    verifyMessage: async ({ message, signature }: SIWEVerifyMessageArgs) => {
+      try {
+        const success = await signIn('credentials', {
+          message,
+          redirect: false,
+          signature,
+          callbackUrl: '/protected'
+        })
+  
+        return Boolean(success?.ok)
+      } catch (error) {
+        return false
+      }
+    },
+    signOut: async () => {
+      try {
+        await signOut({
+          redirect: false
+        })
+  
+        return true
+      } catch (error) {
+        return false
+      }
+    }
+  });
--- context/index.tsx
+++ context/index.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import React, { ReactNode } from 'react'
-import { config, projectId, metadata } from '@/config'
+import { config, projectId, siweConfig } from '@/config'
 
 import { createWeb3Modal } from '@web3modal/wagmi/react'
 
@@ -16,8 +16,8 @@ if (!projectId) throw new Error('Project ID is not defined')
 
 // Create modal
 createWeb3Modal({
-  metadata,
-  wagmiConfig: config,
+  siweConfig,
+  wagmiConfig: config, 
   projectId,
   enableAnalytics: true // Optional - defaults to your Cloud configuration
 })
