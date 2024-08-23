85aada18de84497058879172c6f8139317eeb28f
flo <flo.schwend@gmail.com>
2024-08-23 11:04:53 +0200

    separate login

---
+++ app/login/page.tsx
@@ -0,0 +1,9 @@
+import ConnectButton from '@/app/components/ConnectButton'
+
+export default function Home() {
+  return (
+    <main>
+     <ConnectButton />
+    </main>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import Image from "next/image";
-import ConnectButton from '@/app/components/ConnectButton'
+
 
 export default function Home() {
-  return (
-    <main>
-     <ConnectButton />
-    </main>
-  );
-}
+    return (
+      <main>
+        start page
+      </main>
+    );
+  }
+  
