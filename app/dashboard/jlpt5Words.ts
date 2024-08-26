2b0abe4b7a3807fa0f158c4019274eedb594bd0b
flo <flo.schwend@gmail.com>
2024-08-26 08:23:28 +0200

    fix typo in 2nd word list

--- app/dashboard/jlpt5Words.ts
+++ app/dashboard/jlpt5Words.ts
@@ -1,6 +1,6 @@
 // jlpt5Words.ts
 
 export const jlpt5Words = [
-    { english: 'hello', romanji: 'konnichiwa', hiragana: 'こんにちは', kanji: 'N/A' },
+    { english: 'hello', romanji: 'konnichiwa', hiragana_katakana: 'こんにちは', kanji: 'N/A' },
     // ... add more JLPT-5 words here as they become available
   ];
