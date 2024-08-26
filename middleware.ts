1bf47152f386c97b2f9e3b245cc0fa737c902c7c
flo <flo.schwend@gmail.com>
2024-08-26 11:28:51 +0200

    fix login system

--- middleware.ts
+++ middleware.ts
@@ -1,31 +1,18 @@
-/*
-// This middleware checks if the user is authenticated
-import { NextResponse } from 'next/server';
-
-export function middleware(request) {
-  const { pathname } = request.nextUrl;
-  
-
-  // if pathname is not the authentication route and token does not exist in the cookies
-  if (pathname !== '/login' && !request.cookies.get('auth-token')) {
-    return NextResponse.redirect('/login');
-  }
-
-  return NextResponse.next();
-}
-  */
-
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
- 
-// This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
-  //return NextResponse.redirect(new URL('/home', request.url))
-  console.log(`Incoming request for ${request.nextUrl.pathname}`);
-  return NextResponse.next();
+import { getToken } from 'next-auth/jwt'
+
+export async function middleware(request: NextRequest) {
+  const token = await getToken({ req: request as any, secret: process.env.NEXTAUTH_SECRET })
+  const isAuthPage = request.nextUrl.pathname.startsWith('/login')
+
+  if (!token && !isAuthPage) {
+    return NextResponse.redirect(new URL('/login', request.url))
+  }
+
+  return NextResponse.next()
 }
- 
-// See "Matching Paths" below to learn more
+
 export const config = {
-  matcher: '/about/:path*',
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
 }
